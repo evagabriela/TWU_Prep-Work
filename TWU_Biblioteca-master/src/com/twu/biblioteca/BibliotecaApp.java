aea771c51b83d27ea5062079472447a5758c11c0
Eva Zamudio <gabriela.osu@gmail.com>
2014-04-23 14:36:16 -0400

    Add code to test should welcome message and also refactor test code

--- TWU_Biblioteca-master/src/com/twu/biblioteca/BibliotecaApp.java
+++ TWU_Biblioteca-master/src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,9 +3,11 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        bibliotecaApp.getWelcomeMessage();
     }
 
     public void getWelcomeMessage() {
+        System.out.println("Welcome to the Biblioteca");
     }
 }
--- TWU_Biblioteca-master/test/com/twu/biblioteca/BibliotecaAppTests.java
+++ TWU_Biblioteca-master/test/com/twu/biblioteca/BibliotecaAppTests.java
@@ -2,27 +2,34 @@ package com.twu.biblioteca;
 
 
 import org.junit.Test;
+
+
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import java.io.*;
 
+
 public class BibliotecaAppTests {
-    private BibliotecaApp bibliotecaApp;
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    BibliotecaApp bibliotecaApp;
+    final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
 
     public void setUp() throws Exception {
-     System.setOut(new PrintStream(outContent));
+     System.setOut(new PrintStream(out));
+     bibliotecaApp = new BibliotecaApp();
     }
 
     public void tearDown() throws Exception {
-     System.setOut(null);
+        System.setOut(null);
     }
 
 //    Welcome Message
     @Test
-    public void ShouldWelcomeWhenStartApplication() {
+    public void ShouldWelcomeWhenStartApplication() throws Exception {
+        System.setOut(new PrintStream(out));
         bibliotecaApp = new BibliotecaApp();
         bibliotecaApp.getWelcomeMessage();
-        assertEquals("Welcome to the Biblioteca", outContent.toString());
+        assertThat(out.toString(), containsString("Welcome to the Biblioteca"));
     }
 }
