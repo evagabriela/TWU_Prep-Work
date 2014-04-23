64b1db8a783b81ed600866cc29858498935c4feb
Eva Zamudio <gabriela.osu@gmail.com>
2014-04-23 15:02:00 -0400

    add code for the test of should list of books

--- TWU_Biblioteca-master/src/com/twu/biblioteca/BibliotecaApp.java
+++ TWU_Biblioteca-master/src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,10 +3,13 @@ package com.twu.biblioteca;
 import java.util.ArrayList;
 
 public class BibliotecaApp {
+    private ArrayList listOfBooks = new ArrayList<String>();
 
     public static void main(String[] args) {
         BibliotecaApp bibliotecaApp = new BibliotecaApp();
+
         bibliotecaApp.getWelcomeMessage();
+        System.out.print(bibliotecaApp.getListOfBooks());
     }
 
     public void getWelcomeMessage() {
@@ -14,5 +17,7 @@ public class BibliotecaApp {
     }
 
     public ArrayList getListOfBooks() {
+//        Animal myPet = new Dog();
+        return listOfBooks;
     }
 }
