0e9608bb3d1f97337d262159a30cc2b6b01795fa
Eva Zamudio <gabriela.osu@gmail.com>
2014-04-24 16:17:40 -0400

    add refactoring

--- TWU_Biblioteca-master/test/com/twu/biblioteca/BookTest.java
+++ TWU_Biblioteca-master/test/com/twu/biblioteca/BookTest.java
@@ -10,7 +10,7 @@ import static junit.framework.TestCase.assertEquals;
  */
 public class BookTest {
     @Test
-    public void ShouldTestBookID(){
+    public void ShouldCreateBooks(){
         Book book1 = new Book("Running with the Giants", "John Maxwell");
         Book book2 = new Book("Developing the leader within You", "John Maxwell");
 
